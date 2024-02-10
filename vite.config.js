890ef656a6ec56ae9e6ac14451c5c5bcb9d8f16c
Артём Суворов <artyom.suvorov.shigoto@gmail.com>
2024-02-10 20:33:52 +0300

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import handlebars from 'vite-plugin-handlebars'
 import {resolve} from 'path'
 
 export default defineConfig({
+    base: '/mister-bid',
     root: 'src',
     plugins: [handlebars({
         partialDirectory: resolve(__dirname, 'src/partials')
