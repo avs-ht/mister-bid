2d6203f2103fcde41d4260dc74bfc389aa2e74c2
avsuvorov <artyom.suvorov.shigoto@gmail.com>
2024-02-10 15:18:27 +0300

    first commit (header, main, how-work sections)

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,14 @@
 import '../css/main.css';
+
+const burger = document.querySelector('.burger');
+const header = document.querySelector('.header');
+const body = document.querySelector('body')
+
+burger.addEventListener('click', () => {
+    header.classList.toggle('is-menu-opened');
+    if (body.style.overflow === '') {
+        body.style.overflow = 'hidden';
+        return
+    }
+    body.style.overflow = '';
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    './src/index.html',
+    './src/partials/**/*.{html,hbc}',
+    './src/js/**/*.js'
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'secondary-white': "#F6F7F8",
+        'ocean': '#41BADC',
+        'water': "#435EBE"
+      },
+      borderRadius: {
+        'standart': "7px"
+      }
+    },
+
+
   },
   plugins: [],
 }
